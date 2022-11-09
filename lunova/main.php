31aa6a2c98dda8fb3612d76c06a8267c20a2424d
luibart <bartolomeoluigi85@gmail.com>
2022-11-09 18:55:00 +0100

    main

--- lunova/main.php
+++ lunova/main.php
@@ -18,9 +18,14 @@ require_once "Entity/EVotazione.php";
 require_once "Foundation/FVotazione.php";
 require_once "Foundation/FPersistentManager.php";
 
-//$utt = new EClient("luigi","bart","via ciao","1","Vzzano","AQ","67051","1234567890","l@l.com","passwd",null);
+$utt1 = new EClient("Valentina","Scimia","via vale","3","L'Aquila","AQ","67100","1029384756","valentina@scimia.com",'passwd3!');
+$utt2 = new EClient("Noemi","Barbaro","via noemi","2","L'Aquila","AQ","67100","0987654321","noemi@barbaro.com",'passwd2!');
+$utt3 = new EClient("luigi","Bartolomeo","via marruvio","1","avezzano","AQ","67051","1234567890","l@l.com",'passwd1!');
 
-//$a=FCliente::store($utt);
+$a=FCliente::store($utt1);
+$a=FCliente::store($utt2);
+$a=FCliente::store($utt3);
+
 
 //$a=FCliente::load('l@l.com');
 
@@ -39,11 +44,11 @@ require_once "Foundation/FPersistentManager.php";
 
 //$b=new ERichiesta('dc4',"2022-10-10");
 
-$a = FPersistentManager::getInstance();
-$c = '1';
-$b = $a->prelevaSondaggi();
+//$a = FPersistentManager::getInstance();
+//$b = $a->prelevaOrdini('ut1');
+//print_r($b);
 
-print_r($b);
+
 
 
 
